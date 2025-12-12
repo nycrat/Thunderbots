f4daaee75948eb815ea5f3a0d47057608f1fb415
Eric Xiao <ericxiao0000@gmail.com>
2025-12-12 00:21:46 -0800

    Refactor AngularVelocity into child class of Angle

--- src/software/geom/angular_velocity.h
+++ src/software/geom/angular_velocity.h
@@ -1,11 +1,68 @@
+#pragma once
+
 #include "software/geom/angle.h"
 
 /**
- * We also use variables of type 'Angle' to represent angular velocities, since they
- * are essentially represented the same. This typedef allows us to refer to Angles as
- * AngularVelocities, which makes the interfaces more intuitive.
- * TODO (#3093): Not all methods of Angle class make sense for AngularVelocity!
- *  E.g. Angle::clamp does not make sense in the context of AngularVelocity, as 360 deg/s
- *  is different from 0 deg/s, but 360 deg is the same as 0 deg.
+ * A typesafe representation of an angular velocity, using the Angle class.
+ *
+ * This class helps prevent accidentally combining values in degrees and radians
+ * per second without proper conversion.
+ *
  */
-using AngularVelocity = Angle;
+class AngularVelocity : public Angle
+{
+   public:
+    explicit constexpr AngularVelocity();
+
+    static constexpr AngularVelocity zero();
+    static constexpr AngularVelocity fromRadians(double rad);
+    static constexpr AngularVelocity fromDegrees(double deg);
+
+    constexpr AngularVelocity abs() const;
+
+    // NOTE: kind of weird we have two ways of create zero angle / angular_velocity
+    // static constexpr Angle zero()                 = delete;
+
+    // Delete methods that do not apply to angular velocity
+    static constexpr Angle quarter()              = delete;
+    static constexpr Angle half()                 = delete;
+    static constexpr Angle threeQuarter()         = delete;
+    static constexpr Angle full()                 = delete;
+    static Angle asin(double)                     = delete;
+    static Angle acos(double)                     = delete;
+    static Angle atan(double)                     = delete;
+    constexpr Angle mod(Angle) const              = delete;
+    constexpr Angle remainder(const Angle&) const = delete;
+    double sin() const                            = delete;
+    double cos() const                            = delete;
+    double tan() const                            = delete;
+    constexpr Angle clamp() const                 = delete;
+    constexpr Angle minDiff(const Angle&) const   = delete;
+
+   private:
+    explicit constexpr AngularVelocity(double rads);
+};
+
+inline constexpr AngularVelocity::AngularVelocity() : Angle(0.0) {}
+
+inline constexpr AngularVelocity::AngularVelocity(double rads) : Angle(rads) {}
+
+inline constexpr AngularVelocity AngularVelocity::zero()
+{
+    return AngularVelocity(0.0);
+}
+
+inline constexpr AngularVelocity AngularVelocity::fromRadians(double rad)
+{
+    return AngularVelocity(rad);
+}
+
+inline constexpr AngularVelocity AngularVelocity::fromDegrees(double deg)
+{
+    return AngularVelocity::fromRadians(deg / 180.0 * M_PI);
+}
+
+inline constexpr AngularVelocity AngularVelocity::abs() const
+{
+    return AngularVelocity::fromRadians(toRadians() < 0 ? -toRadians() : toRadians());
+}
