376da0e4ca34f34212b19687bc6a0b65e572b35b
Avah Xiao <avahxiao@gmail.com>
2026-08-19 11:00:33 -0700

    Rewrite python import_all_protos imports with alias

--- src/proto/message_translation/tbots_protobuf.py
+++ src/proto/message_translation/tbots_protobuf.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from proto.import_all_protos import *
+import proto.import_all_protos as protos
 import software.python_bindings as tbots_cpp
 import numpy
 import math
@@ -13,7 +13,7 @@ def create_world_state(
     ball_velocity: tbots_cpp.Vector,
     blue_robot_orientations: list[tbots_cpp.Angle] = [],
     blue_robot_velocities: list[tbots_cpp.Vector] = [],
-) -> WorldState:
+) -> protos.WorldState:
     """Initializes the world from a list of robot locations and ball location/velocity.
 
     NOTE: (index is robot id)
@@ -25,15 +25,15 @@ def create_world_state(
     :param blue_robot_orientations: A list of blue robots orientations
     :param blue_robot_velocities: A list of blue robots velocities
     """
-    world_state = WorldState()
+    world_state = protos.WorldState()
 
     for robot_id, robot_location in enumerate(yellow_robot_locations):
         world_state.yellow_robots[robot_id].CopyFrom(
-            RobotState(
-                global_position=Point(
+            protos.RobotState(
+                global_position=protos.Point(
                     x_meters=robot_location.x(), y_meters=robot_location.y()
                 ),
-                global_orientation=Angle(radians=math.pi),
+                global_orientation=protos.Angle(radians=math.pi),
             )
         )
 
@@ -52,8 +52,8 @@ def create_world_state(
             pass
 
         world_state.blue_robots[robot_id].CopyFrom(
-            RobotState(
-                global_position=Point(
+            protos.RobotState(
+                global_position=protos.Point(
                     x_meters=robot_location.x(), y_meters=robot_location.y()
                 ),
                 global_orientation=tbots_cpp.createAngleProto(orientation),
@@ -62,11 +62,11 @@ def create_world_state(
         )
 
     world_state.ball_state.CopyFrom(
-        BallState(
-            global_position=Point(
+        protos.BallState(
+            global_position=protos.Point(
                 x_meters=ball_location.x(), y_meters=ball_location.y()
             ),
-            global_velocity=Vector(
+            global_velocity=protos.Vector(
                 x_component_meters=ball_velocity.x(),
                 y_component_meters=ball_velocity.y(),
             ),
@@ -76,7 +76,7 @@ def create_world_state(
     return world_state
 
 
-def create_default_world_state(num_robots: int) -> WorldState:
+def create_default_world_state(num_robots: int) -> protos.WorldState:
     """Create a WorldState proto with num_robots yellow and blue robots evenly spaced in two parallel lines on the field.
 
     :param num_robots: Number of robots for the yellow and blue teams
